46b5c0ccf79d72c7d3746a4f92dea4a8beaa6b66
gonogungun <margonoharry987@gmail.com>
2018-03-25 23:06:52 +0700

    push one

---
+++ app/src/main/java/com/example/w3rk/harrimargono_1202151230_modul5/AddData.java
@@ -0,0 +1,42 @@
+package com.example.w3rk.harrimargono_1202151230_modul5;
+
+/**
+ * Created by W3RK$ PC on 3/24/2018.
+ */
+
+public class AddData {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddData(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
